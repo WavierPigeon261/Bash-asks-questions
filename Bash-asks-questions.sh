62f7e4cedc9a288705429bf75e5d987c2561a5a8
WavierPigeon261 <160525160+WavierPigeon261@users.noreply.github.com>
2024-07-26 15:33:15 +0530

    Create Bash-asks-questions.sh
    Just a bash script that asks questions

---
+++ Bash-asks-questions.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# JUST A SIMPLE FILE MADE BY WAVIERPIGEON261
+# GREETING
+echo "Hi!"
+sleep 1
+# ASKING QUESTIONS
+echo "What is your name?"
+# ANSWER
+read name
+# SENTENCE TO SHOW THAT IT IS LISTENING
+echo "Oh. Hi $name,"
+# ASKING AGE
+echo "What is your age?"
+read age
+# FINAL ANSWER FROM BASH
+echo "Hi $name! Your'e $age years old."
+sleep 0.5
+echo ", Thinking how I know that? I know everything!"
+sleep 0.5
+echo "Thanks for using me."
+echo "Bye from WavierPigeon261"
